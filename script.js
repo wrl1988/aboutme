42dcd72a10694551483a06873bb24992186853be
JEFFREY A. OSTLUND <JEFFOSTLUND5@GMAIL.COM>
2020-09-01 17:49:08 -0700

    moving stuff

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+console.log('JS loaded');
+
+function valid_input() {
+  var quizProceed =
+
+
+
+}
+
+var userName = prompt('Welcome to my biography questionnaire! Who am I quizzing today?');
+userName = userName.charAt(0).toUpperCase() + userName.slice(1);
+console.log(userName) === "string";
+alert('Hello ' + userName + '! Good luck on your super serious Jeff Ostlund Final.');
+
+var askBio = prompt(userName + ', would you guess that I am 30 or 31?');
+if (askBio = 30) {
+  console.log('Sorry' + userName + ', but you will have to guess again.');
+  alert('Sorry' + userName + ', but you will have to guess again.');
+}
+else if (askBio = 31) {
+  console.log('Way to go' + userName + '! That was easy though. Lets see whats next.');
+  alert('Way to go ' + userName + '! That was easy though Lets see whats next.');
+}
+  
+
+var askBio = prompt('' + userName + '' + favFood + '');
+console.log('O' + userHobby);
+alert(userHobby + '');
+
+var askEdu = prompt(userName + '' + favFood + '' + userHobby + '');
+console.log('' + favFood + '' + userHobby);alert('');
+
+var askWork = prompt(userName + '' + favFood + '' + userHobby + '');
+console.log('' + favFood + '' + userHobby);
+alert('');
+
+var askGoal = prompt(userName + '' + favFood + '' + userHobby + '');
+console.log('' + favFood + '' + userHobby);
+alert('');
+
